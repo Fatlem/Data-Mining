63012a9d6ef628965cca11d3724a6b5b85df8ae0
FatlemYo <122469156+Fatlem@users.noreply.github.com>
2024-12-15 13:23:29 +0700

    Add files via upload

---
+++ UAS-DM/Test.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #### "Prediksi Risiko Penyakit Jantung Menggunakan Algoritma Klasifikasi"
+
+# ##### 1. Pengumpulan Data
+
+# In[1]:
+
+
+import pandas as pd
+
+# Mengunduh dan membaca dataset
+data = pd.read_csv('Heart_Disease_Classification_Dataset.csv')  # Ganti dengan path file Anda
+
+# Menampilkan beberapa baris pertama dari dataset
+print(data.head())
+
+
+# - Kode ini mengimpor pustaka pandas dan membaca dataset dari file CSV. Kemudian, menampilkan lima baris pertama dari dataset untuk memahami strukturnya.
+
+# ##### 2. Preprocessing Data
+
+# In[2]:
+
+
+# Memeriksa nilai yang hilang
+print(data.isnull().sum())
+
+# Menghapus baris dengan nilai yang hilang (jika ada)
+data.dropna(inplace=True)
+
+# Normalisasi fitur (contoh: kolesterol dan tekanan darah)
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler()
+data[['chol', 'trestbps']] = scaler.fit_transform(data[['chol', 'trestbps']])
+
+# Menampilkan informasi dataset setelah preprocessing
+print(data.info())
+
+
+# - Memeriksa apakah ada nilai yang hilang dalam dataset. Jika ada, baris tersebut dihapus. Kemudian, fitur kolesterol dan tekanan darah dinormalisasi ke rentang 0-1 menggunakan MinMaxScaler. Terakhir, informasi dataset ditampilkan untuk memastikan tidak ada nilai yang hilang.
+
+# ##### 3. Pemilihan Algoritma Klasifikasi
+
+# In[3]:
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+# Memisahkan fitur dan target
+X = data.drop('target', axis=1)  # Fitur
+y = data['target']  # Target
+
+# Membagi data menjadi data pelatihan dan pengujian
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+# - Memisahkan dataset menjadi fitur (X) dan target (y). Kemudian, data dibagi menjadi data pelatihan dan pengujian dengan proporsi 80% untuk pelatihan dan 20% untuk pengujian.
